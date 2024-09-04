17046be534a05ab36c7b4fa7d6100398939bfda8
Akerele Rofih <rofihakerele@gmail.com>
2024-09-04 08:21:09 +0100

    assignment

---
+++ javaAssignment/Divisible.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Divisible{
+	public static void main(String[] args){
+		for(int number=100;number<200;number++){
+			if(number%3==0){
+				System.out.printf("%d ",number);
+			}
+			
+		}
+		for(int number=100;number<200;number++){
+			if(number%3==0){
+				System.out.printf(" %d ",number);
+			}
+
+		}
+
+		
+	}
+} 
