662f1820c841bc46892948ec44448933b2846378
Akerele Rofih <rofihakerele@gmail.com>
2024-09-06 08:37:49 +0100

    assign

---
+++ javaAssignment/LetterCounter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class LetterCounter{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	System.out.println("enter your name");
+
+	String str = input.next();
+	int length = str.length();
+
+	System.out.println("number of letters are " + length); 
+	
+	} 
+}
---
+++ javaAssignment/PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PrimeNumber{
+	public static void main(){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int value=input.nextInt();
+		isPrimeMethod(value);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+public static boolean isPrimeMethod(int number){
+	if(number%2==0)
+		return True
+
+
+
+
+}
+}
